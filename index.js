acdbeb9824435da43c0a8e544ed0c6eb00a00ccc
bennyyyy-x <89168678+bennyyyy-x@users.noreply.github.com>
2024-01-25 07:03:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const translate = require('./translate.js');
 const redirect = require('./redirect.js');
 
 app.get('/', function(req, res) {
-    res.render('page', { msg: 'Random Paragraph' });
+    res.render('page', { msg: '' });
 });
 
 app.set('view engine', 'pug');
@@ -21,12 +21,18 @@ app.use(express.static('views'));
 
 app.post('/', function(req, res) {
     var long_url = req.body.long;
-
-    mdb.find_or_store(long_url).then(
-        function(value) {
-            res.render('page', { msg: (req.get('host') + req.originalUrl + value) });
-        }
-    )
+    if (!/^https?:\/\//i.test(long_url)) {
+        long_url = 'http://' + long_url;
+    }
+    if (/^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test( long_url )) {
+        mdb.find_or_store(long_url).then(
+            function(value) {
+                res.render('page', { msg: (req.get('host') + req.originalUrl + value) });
+            }
+        )
+    } else {
+        res.render('page', { msg: 'Invalid URL Entered' });
+    }
 });
 
 app.get('/:id', function(req, res) {
@@ -39,9 +45,6 @@ app.get('/:id', function(req, res) {
             if (value == '!') {
                 res.status(404).json('Not Found');
             } else {
-                if (!/^https?:\/\//i.test(value)) {
-                    value = 'http://' + value;
-                }
                 res.redirect(value);
             }
         }
