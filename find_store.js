8dcf224d7128c96ca433cc517d8aee64743aa3b8
bennyyyy-x <89168678+bennyyyy-x@users.noreply.github.com>
2024-01-25 07:04:13 -0500

    Update find_store.js

--- find_store.js
+++ find_store.js
@@ -1,31 +1,28 @@
-const MongoClient = require("mongodb");
-const url = "mongodb+srv://bennyxu:T3eL8KOMydkKc6Ya@cluster0.h4t5cmh.mongodb.net/?retryWrites=true&w=majority";
+const MongoClient = require('mongodb');
+const url = 'mongodb+srv://bennyxu:T3eL8KOMydkKc6Ya@cluster0.h4t5cmh.mongodb.net/?retryWrites=true&w=majority';
 const client = new MongoClient.MongoClient(url);
 
-const translate = require("./translate.js");
+const translate = require('./translate.js');
 
 async function find_or_store(long_url) {
     try {
 
-        const cluster = client.db("Cluster0");
-        const collection = cluster.collection("urls");
+        const database = client.db('Cluster0');
+        const collection = database.collection('urls');
 
         var query = { long_url: long_url }
         var result = await collection.findOne(query);
 
         if (result) { // website already stored in database
 
-            var short_url = translate.hex_to_url(result._id.toString("hex"));
-            // console.log("old short_url is " + short_url);
+            var short_url = translate.hex_to_url(result._id.toString('hex'));
             return short_url;
 
         } else { // new website
 
             var website = { long_url: long_url };
             var obj = await collection.insertOne(website);
-
-            var short_url = translate.hex_to_url(obj.insertedId.toString("hex"));
-            // console.log("new short_url is " + short_url);
+            var short_url = translate.hex_to_url(obj.insertedId.toString('hex'));
             return short_url;
 
         }
