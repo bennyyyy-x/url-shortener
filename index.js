d6a756ae1fb50b6a3916b9e3277b6ed3869ae23e
bennyyyy-x <89168678+bennyyyy-x@users.noreply.github.com>
2024-01-25 05:54:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
-const path = require("path");
 const upload = multer();
 const app = express();
 
-app.get('/', function(req, res){
-    res.render('page', { msg: "Random Paragraph" });
+const mdb = require('./find_store.js');
+const translate = require('./translate.js');
+const redirect = require('./redirect.js');
+
+app.get('/', function(req, res) {
+    res.render('page', { msg: 'Random Paragraph' });
 });
 
 app.set('view engine', 'pug');
@@ -16,13 +19,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array());
 app.use(express.static('views'));
 
-const mdb = require("./find_store.js");
-
-app.post('/', function(req, res){
+app.post('/', function(req, res) {
     var long_url = req.body.long;
-    var msg = mdb.find_or_store(long_url).then(function(value) {
-        res.render("page", { msg: (req.get('host') + req.originalUrl + value) });
-    });
+
+    mdb.find_or_store(long_url).then(
+        function(value) {
+            res.render('page', { msg: (req.get('host') + req.originalUrl + value) });
+        }
+    )
 });
+
+app.get('/:id', function(req, res) {
+    var url = req.params.id;
+
+    var db_id = translate.url_to_hex(url);
+
+    redirect.get_url(db_id).then(
+        function(value) {
+            if (value == '!') {
+                res.status(404).json('Not Found');
+            } else {
+                if (!/^https?:\/\//i.test(value)) {
+                    value = 'http://' + value;
+                }
+                res.redirect(value);
+            }
+        }
+    )
+})
 
 app.listen(8080);
